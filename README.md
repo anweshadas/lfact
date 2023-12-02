8d96a60227b00502d2230761248cc8fda9c2aaa1
Anwesha Das <anwesha@das.community>
2023-12-02 16:32:13 +0100

    Adds initial code for example project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an example project from Pym book to learn Github Actions.
---
+++ lfact/__init__.py
@@ -0,0 +1,6 @@
+" This is docstring"
+
+__version__ = "0.1.0"
+
+from .fact import factorial,cli
+__all__ = [factorial, cli, __version__]
